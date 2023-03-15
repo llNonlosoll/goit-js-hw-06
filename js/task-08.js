b19822be375fbd24048d1c08593c2ca58f2cc25c
llNonlosoll <wishmasterar@gmail.com>
2023-03-15 19:15:44 +0100

    08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
+console.log(form);
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = {
+    email,
+    password,
+  };
+
+  if (formElements.email.value !== '' && formElements.password.value !== '') {
+    console.log(formData);
+    return form.reset();
+  }
+  alert('Я тобі кажу, що всі поля повинні бути заповнені!!!');
+}
