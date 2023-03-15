a24a34546173891a456c84936681a3985be906f8
llNonlosoll <wishmasterar@gmail.com>
2023-03-15 14:46:41 +0100

    task 02 done

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,7 @@ const ulCategories = document.querySelector('#categories');
 const ulChildren = ulCategories.children;
 console.log('Number of categories: ', ulChildren.length);
 
-const listTotal = [...ulChildren].forEach(element => {
+const finalMessage = [...ulChildren].forEach(element => {
   console.log('Category: ', element.firstElementChild.textContent);
   console.log('Elements: ', element.lastElementChild.children.length);
 });
