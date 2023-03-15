96184288aa460ff89efbf2aeb69a12acb15f79a6
llNonlosoll <wishmasterar@gmail.com>
2023-03-15 20:54:23 +0100

    09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const bodyForColor = document.querySelector('body');
+
+const button = document.querySelector('.change-color');
+
+const colorText = document.querySelector('.color');
+
+button.addEventListener('click', getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  const color = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+  bodyForColor.style.backgroundColor = color;
+  colorText.textContent = color;
+  return console.log(color);
 }
